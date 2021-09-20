3c2cfa9cdbf8f4b46f0252422c26e385f82be221
GideobOgbuagu <reachgiddy@gmail.com>
2021-09-20 17:45:12 -0400

    added custom styling to navbar & created footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import Head from 'next/head'
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core'
+import useStyles from '../utils/styles'
+
 
 function Layout({children}) {
+  const classes = useStyles()
   return (
     <div>
       <Head>
         <title>Next Shop</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Next Shop</Typography>
         </Toolbar>
       </AppBar>
-      <Container>
+      <Container className={classes.main}>
         {children}
       </Container>
+      <footer className={classes.footer}>
+        <Typography>
+          All right reserved. Next Shop
+        </Typography>
+      </footer>
     </div>
   )
 }
