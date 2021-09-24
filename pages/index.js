ca5277b8444088c4e43da476af54774383860956
GideobOgbuagu <reachgiddy@gmail.com>
2021-09-24 13:23:13 -0400

    displaying custom data with materialui and react

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,39 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@material-ui/core'
+import data from '../utils/data';
 import Layout from '../components/Layout'
 
 function Home() {
   return (
     <Layout>
-        <div>
+      <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia 
+                  component="img"
+                  image={product.image}
+                  title={product.name}
+                  >
+                  </CardMedia>
+                  <CardContent>
+                    <Typography>
+                      {product.name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" >
+                    Add to Cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
  
