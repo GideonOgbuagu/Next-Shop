04405dd170867deda5f2f2ac87b4e3a830218d04
GideobOgbuagu <reachgiddy@gmail.com>
2021-09-24 13:50:26 -0400

    added cart and login navlinks

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Head from 'next/head'
-import { AppBar, Toolbar, Typography, Container } from '@material-ui/core'
-import useStyles from '../utils/styles'
+import React from "react";
+import Head from "next/head";
+import NavLink from "next/link";
+import { AppBar, Toolbar, Typography, Container, Link } from "@material-ui/core";
+import useStyles from "../utils/styles";
 
-
-function Layout({children}) {
-  const classes = useStyles()
+function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
@@ -13,19 +13,28 @@ function Layout({children}) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Next Shop</Typography>
+          <NavLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Next Shop</Typography>
+            </Link>
+          </NavLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NavLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NavLink>
+            <NavLink href="/login" passHref>
+              <Link>Login</Link>
+            </NavLink>
+          </div>
         </Toolbar>
       </AppBar>
-      <Container className={classes.main}>
-        {children}
-      </Container>
+      <Container className={classes.main}>{children}</Container>
       <footer className={classes.footer}>
-        <Typography>
-          All right reserved. Next Shop
-        </Typography>
+        <Typography>All right reserved. Next Shop</Typography>
       </footer>
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
--- utils/styles.js
+++ utils/styles.js
@@ -1,19 +1,26 @@
-import { makeStyles } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
   navbar: {
-    backgroundColor: '#203040',
-    '& a': {
-      color: '#ffffff',
+    backgroundColor: "#203040",
+    "& a": {
+      color: "#ffffff",
       marginLeft: 10,
     },
   },
+  brand: {
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+  },
+  grow: {
+    flexGrow: 1,
+  },
   main: {
-    minHeight: '80vh'
+    minHeight: "80vh",
   },
   footer: {
-    textAlign: 'center'
-  }
-})
+    textAlign: "center",
+  },
+});
 
 export default useStyles;
